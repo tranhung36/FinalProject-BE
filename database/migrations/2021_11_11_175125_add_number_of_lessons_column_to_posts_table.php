41d72ce5513027c9d8ca46323657fc4012e48f88
Tran Hung <tranqhung36@gmail.com>
2021-11-12 00:53:00 +0700

    fixed

---
+++ database/migrations/2021_11_11_175125_add_number_of_lessons_column_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNumberOfLessonsColumnToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('number_of_lessons')->default(1);
+            $table->integer('number_of_weeks')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
